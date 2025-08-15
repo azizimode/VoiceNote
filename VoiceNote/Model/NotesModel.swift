172bb7f890e28c581a4c801c35c62c3ffc047f77
MOD <aziziran9@gmail.com>
2025-08-15 14:52:38 +0330

    Initial Commit

---
+++ VoiceNote/Model/NotesModel.swift
@@ -0,0 +1,12 @@
+//
+//  NotesModel.swift
+//  VoiceNote
+//
+//  Created by Mohammad on 8/15/25.
+//
+
+import Foundation
+
+struct NotesModel {
+    var title: String
+}
---
+++ VoiceNote/Views/NotesView.swift
@@ -0,0 +1,41 @@
+//
+//  NotesView.swift
+//  VoiceNote
+//
+//  Created by Mohammad on 8/15/25.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                List{
+                    Text("Hello, World!")
+                }
+                .navigationTitle("Notes")
+                .listStyle(.automatic)
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Button{}label: {
+                            Image(systemName: "microphone")
+                                .font(.system(size: 32))
+                                .frame(width: 80, height: 80)
+                                .background(Color.blue)
+                                .cornerRadius(40)
+                                .foregroundColor(.white)
+                                .shadow(color:Color.blue.opacity(0.8) ,radius: 8)
+                        }
+                    }
+                    .padding(.bottom, 20)
+                }
+            }
+        }
+    }
+}
+#Preview {
+    NotesView()
+}

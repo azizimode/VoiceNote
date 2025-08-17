e3a95a81e0a4c362a062c5b3d7310ee041ec5e98
MOD <aziziran9@gmail.com>
2025-08-17 21:34:28 +0330

    [Feature] SwiftData Added

--- VoiceNote/ViewModel/NoteViewModel.swift
+++ VoiceNote/ViewModel/NoteViewModel.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
+import SwiftData
 import AVFoundation
 
+@Model
 class AudioRecorder: NSObject, ObservableObject, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 //    @Published var recordings : [Recording] = []
 //    @Published var audioLevels: [CGFloat] = Array(repeating: 20, count: 30)
@@ -15,4 +17,15 @@ class AudioRecorder: NSObject, ObservableObject, AVAudioRecorderDelegate, AVAudi
 //    init(note: NotesView) {
 //        self.note = AudioNoteModel(title: "Hey this my first voice note")
 //    }
+    var title: String
+    var text: String
+    var date: Date
+    
+    
+    
+    init(title: String, text: String, date: Date) {
+        self.title = title
+        self.text = text
+        self.date = date
+    }
 }
--- VoiceNote/VoiceNoteApp.swift
+++ VoiceNote/VoiceNoteApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct VoiceNoteApp: App {
@@ -13,5 +14,6 @@ struct VoiceNoteApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: AudioRecorder.self)
     }
 }
